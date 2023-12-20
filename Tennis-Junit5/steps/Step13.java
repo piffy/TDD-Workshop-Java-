cbb829270e517fff0c68aed58558f913e10b6605
Marcello Missiroli <prof.missiroli@gmail.com>
2023-12-20 11:56:43 +0100

    Update Step13.java

--- Tennis-Junit5/steps/Step13.java
+++ Tennis-Junit5/steps/Step13.java
@@ -1,10 +1,20 @@
 Create TieBreak, una classe derivata di Game
 
-Quindi create il relativo test tramite il comando
+Quindi create il relativo test tramite il comando (oramai dovreste sapere come si fa, ad ogni modo)
+	NETBEANS:
 	Click destro su TieBreak.java
 	Tools > Create/Update Test 
 	Click su [x] Public methods > [OK] > Junit 4.x > [Select]
 
+        
+	Click destro su Game.java
+	Tools > Create/Update Test 
+	Click su [x] Public methods > [OK] > Junit 4.x > [Select]
+	
+	IDEA:
+	Click destro sul nome del progetto > New > Directory    e chiamatela test 
+	Click destro su directory test > Mark directory as      Test sources Root
+	Nel file TieBreak.java Click destro sulla PAROLA TieBreak  e premere [Alt]-[Invio] > Create test	
 
 SostituiteTieBreakTest con questo
 
